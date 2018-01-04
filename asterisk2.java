5a0dc06067a387dd4127173890bc8d441707082e
David Yarnall <davidyarnall@Davids-Air.home>
2018-01-04 10:13:02 -0500

    final version asterisk2

---
+++ asterisk2.java
@@ -0,0 +1,9 @@
+class Asterisk2 {
+    public static void main(String[] args) {
+        int n = 9;
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(“*”);
+        }
+    }
+}
